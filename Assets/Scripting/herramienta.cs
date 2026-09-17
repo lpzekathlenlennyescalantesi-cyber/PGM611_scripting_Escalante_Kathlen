320d816a07c8099014c751160a579efaaeff5308
KathlenEscalante <lpze.kathlenlenny.escalante.si@unifranz.edu.bo>
2026-09-17 16:42:42 -0400

    Primer comit del proyecto

---
+++ Assets/Scripting/herramienta.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace herramientas.conectividad
+{
+
+    public partial class herramienta
+    {
+        public int contador;
+        public int vida;
+        public int calculoPuntaje()
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Scripting/jugador.cs
@@ -0,0 +1,69 @@
+using logica_de_enemigo.herramientas.calculos;
+using logica_de_jugado;
+using UnityEngine;
+
+// el UnityEngine es un namespace 
+
+namespace logica_de_jugado // dentro de namespace se puede crear mucho namespace 
+{
+
+    public class jugador : MonoBehaviour // : MonoBehaviour es que lo que esta adelante de este es el hijo y el mono es el padre 
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+ namespace logica_de_enemigo
+    {
+
+        public class Enemigo : MonoBehaviour
+        {
+
+        }
+    namespace herramientas
+    {
+        namespace calculos
+        {
+            public class Ejemplo
+            {
+                public void metodoEjemplo()
+                {
+                    jugador j;
+                }
+            }
+        }
+    }
+        namespace conectividad
+        {
+            public partial class Herramienta
+            {
+
+            }
+        }
+    }
+
+namespace logica_de_saltar
+    {
+        namespace caminar
+        {
+            using herramientas.calculos;
+
+            public class caminar
+            {
+                public void metodo_caminar()
+                {
+                    Ejemplo e;
+                }
+
+            }
+        }
+    }
